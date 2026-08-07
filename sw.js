21f3dd3d7e137b1c265d111f5deb462ce909c6bf
vivienneduilio <vivienneduilio@gmail.com>
2026-08-07 12:45:13 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-self.addEventListener("install",e=>e.waitUntil(caches.open("vivi-life-xp").then(c=>c.addAll(["./","./index.html","./manifest.json"])))); self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener("install",e=>e.waitUntil(caches.open("vivi-life-xp-v2").then(c=>c.addAll(["./","./index.html","./manifest.json"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
